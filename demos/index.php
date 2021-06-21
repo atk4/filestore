72ef0d452145ae33ac304ae1bb8486cb6ac543e1
Francesco Danti <fdanti@gmail.com>
2021-06-21 10:29:11 +0200

    Align with Atk 2.4 (#20)
    * Align with atk 2.4
    
    * Add php-cs-fixer
    
    * Align to 2.4 and cleanup
    
    * Add helper class for View and Download of files
    
    * typo old namespace
    
    * Update readme with new namespace
    
    * Renamed basic.php to index.php
    
    * Add headers normalize

---
+++ demos/index.php
@@ -0,0 +1,103 @@
+<?php
+
+declare(strict_types=1);
+
+require '../vendor/autoload.php';
+
+use Atk4\Filestore\Field\File;
+use Atk4\Filestore\Helper;
+use Atk4\Ui\Callback;
+use Atk4\Ui\Columns;
+use Atk4\Ui\Form;
+use Atk4\Ui\JsExpression;
+use League\Flysystem\Adapter\Local;
+use League\Flysystem\Filesystem;
+
+$app = new \Atk4\Ui\App('Filestore Demo');
+$app->initLayout([\Atk4\Ui\Layout\Centered::class]);
+
+// change this as needed
+$app->db = Atk4\Data\Persistence::connect('mysql://root:root@localhost/atk4_filestore');
+
+// specify folder where files will be actually stored
+$adapter = new Local(__DIR__ . '/localfiles');
+$filesystem = new Filesystem($adapter);
+
+class Friend extends \Atk4\Data\Model
+{
+    public $table = 'friend';
+
+    public $filesystem;
+
+    protected function init(): void
+    {
+        parent::init();
+
+        $this->addField('name');                                                     // friend's name
+        $this->addField('file', new File($this->filesystem));  // storing file here
+        $this->addField('file2', new File($this->filesystem)); // storing file here
+    }
+}
+
+$col = Columns::addTo($app);
+
+$form = Form::addTo($col->addColumn());
+$form->setModel(
+    new Friend($app->db, [
+        'filesystem' => $filesystem,
+    ])
+);
+$form->model->tryLoad(1);
+
+$gr = \Atk4\Ui\Grid::addTo($col->addColumn(), [
+    'menu' => false,
+    'paginator' => false,
+]);
+$gr->setModel(new \Atk4\Filestore\Model\File($app->db));
+
+$form->onSubmit(function (Atk4\Ui\Form $form) use ($gr) {
+    $form->model->save();
+
+    return [
+        $gr->jsReload(),
+    ];
+});
+
+\Atk4\Ui\View::addTo($app, ['ui' => 'divider']);
+
+$crud = \Atk4\Ui\Crud::addTo($app);
+$crud->setModel(new Friend($app->db, ['filesystem' => $filesystem]));
+
+\Atk4\Ui\View::addTo($app, ['ui' => 'divider']);
+
+$callback_download = Callback::addTo($app);
+$callback_download->set(function () use ($crud) {
+    $id = $crud->getApp()->stickyGet('row_id');
+    $model = (clone $crud->model);
+    $model_file = $model->load($id)->ref('file');
+    Helper::download($model_file, $crud->getApp());
+});
+
+$crud->addActionButton(
+    ['icon' => 'download'],
+    new JsExpression(
+        'document.location = "' . $callback_download->getJSURL() . '&row_id="+[]',
+        [$crud->table->jsRow()->data('id')]
+    )
+);
+
+$callback_view = Callback::addTo($app);
+$callback_view->set(function () use ($crud) {
+    $id = $crud->getApp()->stickyGet('row_id');
+    $model = (clone $crud->model);
+    $model_file = $model->load($id)->ref('file');
+    Helper::view($model_file, $crud->getApp());
+});
+
+$crud->addActionButton(
+    ['icon' => 'image'],
+    new JsExpression(
+        'document.location = "' . $callback_view->getJSURL() . '&row_id="+[]',
+        [$crud->table->jsRow()->data('id')]
+    )
+);
---
+++ src/Helper.php
@@ -0,0 +1,87 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Atk4\Filestore;
+
+use Atk4\Filestore\Model\File;
+use Atk4\Ui\App;
+
+class Helper
+{
+    public static function download(File $model, App $app = null)
+    {
+        $headers = [
+            'Content-Description' => 'File Transfer',
+            'Content-Type' => 'application/octet-stream',
+            'Cache-Control' => 'must-revalidate',
+            'Expires' => '-1',
+            'Content-Disposition' => 'attachment; filename="' . $model->get('meta_filename') . '"',
+            'Content-Length' => (string) $model->get('meta_size'),
+            'Pragma' => 'public',
+            'Accept-Ranges' => 'bytes',
+        ];
+
+        static::output($model, $headers, $app);
+    }
+
+    private static function output(File $model, array $headers, App $app = null)
+    {
+        $headers = static::normalizeHeaders($headers);
+
+        $location = $model->get('location');
+
+        if ($app !== null) {
+            $app->terminate($model->flysystem->get($location)->read(), $headers);
+        }
+
+        $isCli = \PHP_SAPI === 'cli'; // for phpunit
+
+        foreach ($headers as $k => $v) {
+            if (!$isCli) {
+
+                $kCamelCase = preg_replace_callback('~(?<![a-zA-Z])[a-z]~', function ($matches) {
+                    return strtoupper($matches[0]);
+                }, $k);
+
+                header($kCamelCase . ': ' . $v);
+            }
+        }
+
+        fpassthru($model->flysystem->readStream($location));
+
+        exit;
+    }
+
+    public static function view(File $model, App $app = null)
+    {
+        $headers = [
+            'Content-Description' => 'File Transfer',
+            'Content-Type' => $model->get('meta_mime_type'),
+            'Cache-Control' => 'must-revalidate',
+            'Expires' => '-1',
+            'Content-Disposition' => 'inline; filename="' . $model->get('meta_filename') . '"',
+            'Content-Length' => (string) $model->get('meta_size'),
+            'Pragma' => 'public',
+            'Accept-Ranges' => 'bytes',
+        ];
+
+        static::output($model, $headers, $app);
+    }
+
+    // copied from Atk4/Ui/App
+    private static function normalizeHeaders(array $headers): array
+    {
+        $res = [];
+        foreach ($headers as $k => $v) {
+            if (is_numeric($k) && ($p = strpos($v, ':')) !== false) {
+                $k = substr($v, 0, $p);
+                $v = substr($v, $p + 1);
+            }
+
+            $res[strtolower(trim($k))] = trim($v);
+        }
+
+        return $res;
+    }
+}
