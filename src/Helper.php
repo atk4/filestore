dfbe8f01d951251900441c18c4dc64bcab37d1a2
DarkSide <imants.horsts@inbox.lv>
2023-04-16 13:58:28 +0300

    cleanup and fix Helper

--- src/Helper.php
+++ src/Helper.php
@@ -15,11 +15,28 @@ class Helper
      */
     public static function download(File $model, App $app): void
     {
-        $app->setResponseHeader('Content-Description', 'File Transfer');
-        $app->setResponseHeader('Content-Type', 'application/octet-stream');
+        $app->setResponseHeader('Content-Description', 'Download File');
+        $app->setResponseHeader('Content-Type', $model->get('meta_mime_type'));
         $app->setResponseHeader('Cache-Control', 'must-revalidate');
         $app->setResponseHeader('Expires', '-1');
         $app->setResponseHeader('Content-Disposition', 'attachment; filename="' . $model->get('meta_filename') . '"');
+        $app->setResponseHeader('Content-Length', (string) $model->get('meta_size'));
+        $app->setResponseHeader('Pragma', 'public');
+        $app->setResponseHeader('Accept-Ranges', 'bytes');
+
+        static::output($model, $app);
+    }
+
+    /**
+     * @return never
+     */
+    public static function view(File $model, App $app): void
+    {
+        $app->setResponseHeader('Content-Description', 'View File');
+        $app->setResponseHeader('Content-Type', $model->get('meta_mime_type'));
+        $app->setResponseHeader('Cache-Control', 'must-revalidate');
+        $app->setResponseHeader('Expires', '-1');
+        $app->setResponseHeader('Content-Disposition', 'inline; filename="' . $model->get('meta_filename') . '"');
         $app->setResponseHeader('Content-Length', (string) $model->get('meta_size'));
         $app->setResponseHeader('Pragma', 'public');
         $app->setResponseHeader('Accept-Ranges', 'bytes');
@@ -37,22 +54,5 @@ class Helper
         $stream = (new Psr17Factory())->createStreamFromResource($resource);
 
         $app->terminate($stream);
-    }
-
-    /**
-     * @return never
-     */
-    public static function view(File $model, App $app): void
-    {
-        $app->setResponseHeader('Content-Description', 'File Transfer');
-        $app->setResponseHeader('Content-Type', $model->get('meta_mime_type'));
-        $app->setResponseHeader('Cache-Control', 'must-revalidate');
-        $app->setResponseHeader('Expires', '-1');
-        $app->setResponseHeader('Content-Disposition', 'inline; filename="' . $model->get('meta_filename') . '"');
-        $app->setResponseHeader('Content-Length', (string) $model->get('meta_size'));
-        $app->setResponseHeader('Pragma', 'public');
-        $app->setResponseHeader('Accept-Ranges', 'bytes');
-
-        static::output($model, $app);
     }
 }
