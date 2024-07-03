a32937473ff9d1f75cb8ff9b4237eaca4ef98649
Michael Krecek <michael@krecek.net>
2024-07-03 12:33:33 +0200

    Check existing location tokens on unconditioned model

--- src/Model/File.php
+++ src/Model/File.php
@@ -108,7 +108,7 @@ class File extends Model
         $this->onHookShort(Model::HOOK_AFTER_DELETE, function () {
             $path = $this->get('location');
             if ($path && $this->flysystem->fileExists($path)) {
-                $files = (clone $this->getModel())->addCondition(
+                $files = (new self($this->getModel()->getPersistence()))->addCondition(
                     'id',
                     '!=',
                     $this->getId()
